490f1bc851dd0c1b5268c8248adeca973d595a14
slowstab <36301891+slowstab@users.noreply.github.com>
2021-06-20 03:52:41 +0200

    fix avatars and add nickname support

--- components/StatusHandler.jsx
+++ components/StatusHandler.jsx
@@ -1,12 +1,21 @@
-const { React } = require('powercord/webpack');
-const { Statuses } = require('./../Constants');
+const { React } = require("powercord/webpack");
+const { Statuses } = require("./../Constants");
 
 module.exports = ({ status, user, Avatar }) => (
   <div class="bf-status-popup">
-    <div class='bf-status-userinline'>
-      <Avatar src={user.avatarURL} status={status} size='SIZE_32' statusTooltip={true} />
-      <div style={{ marginLeft: '8px',
-        marginTop: '8px' }} class="bf-status-popup-text">{user.username} is now {Statuses[status].friendly}</div>
+    <div class="bf-status-userinline">
+      <Avatar
+        src={user.getAvatarURL()}
+        status={status}
+        size="SIZE_32"
+        statusTooltip={true}
+      />
+      <div
+        style={{ marginLeft: "8px", marginTop: "8px" }}
+        class="bf-status-popup-text"
+      >
+        {user.username} is now {Statuses[status].friendly}
+      </div>
     </div>
   </div>
 );
--- modules/StatusPopup.js
+++ modules/StatusPopup.js
@@ -1,22 +1,22 @@
 /*eslint-disable*/
-const { inject } = require('powercord/injector');
-const { React, getModule } = require('powercord/webpack');
-const { waitFor, getOwnerInstance } = require('powercord/util');
-const { StatusHandler } = require('./../components');
+const { inject } = require("powercord/injector");
+const { React, getModule } = require("powercord/webpack");
+const { waitFor, getOwnerInstance } = require("powercord/util");
+const { StatusHandler } = require("./../components");
 const { XenoLib } = global;
 
 const FRIENDLY_STATEMENT = {
-  online: 'went online',
-  offline: 'went offline',
-  dnd: 'went in to do not disturb',
-  idle: 'went idle'
+  online: "went online",
+  offline: "went offline",
+  dnd: "went in to do not disturb",
+  idle: "went idle",
 };
 
 const STATUS_COLOR = {
-  online: '#43b581',
-  offline: '#747f8d',
-  dnd: '#f04747',
-  idle: '#faa61a'
+  online: "#3ba55c",
+  offline: "#747f8d",
+  dnd: "#ed4245",
+  idle: "#faa61a",
 };
 
 /*
@@ -25,40 +25,53 @@ const STATUS_COLOR = {
  * Contributors: aetheryx#0001
  */
 module.exports = async function () {
-  if (!this.settings.get('statuspopup', true)) return;
-  const Avatar = await getModule(m => m && m.Sizes && typeof m === 'function' && m.Sizes['SIZE_32'] === 'SIZE_32');
+  if (!this.settings.get("statuspopup", true)) return;
+  const Avatar = await getModule(
+    (m) =>
+      m &&
+      m.Sizes &&
+      typeof m === "function" &&
+      m.Sizes["SIZE_32"] === "SIZE_32"
+  );
   this.createFriendPopup = (user, status) => {
-    const timeout = this.settings.wpmTimeout ? Math.min(this.calculateTime(notif.title) + this.calculateTime(notif.content), 60000) : 0;
+    const timeout = this.settings.wpmTimeout
+      ? Math.min(
+          this.calculateTime(notif.title) + this.calculateTime(notif.content),
+          60000
+        )
+      : 0;
     const notificationId = global.XenoLib.Notifications.show(
       React.createElement(
-        'div',
+        "div",
         {
-          className: 'BF-message'
+          className: "BF-message",
         },
         React.createElement(StatusHandler, {
           status,
           user,
-          Avatar
+          Avatar,
         })
       ),
       {
         timeout: Math.max(5000, timeout),
-        color: STATUS_COLOR[status]
+        color: STATUS_COLOR[status],
       }
     );
-    
-    if (!this.settings.get('osNotif', true)) return;
-    new Notification(`${user.username} ${FRIENDLY_STATEMENT[status]}.`, {icon:user.avatarURL})
-  };
-  const { getStatus } = await getModule([ 'getStatus' ]);
-  const getUser = await getModule([ 'getUser', 'getCurrentUser' ]);
 
-  inject('bf-user', getUser, 'getUser', (args, res) => {
+    if (!this.settings.get("osNotif", true)) return;
+    new Notification(`${user.username} ${FRIENDLY_STATEMENT[status]}.`, {
+      icon: user.getAvatarURL(),
+    });
+  };
+  const { getStatus } = await getModule(["getStatus"]);
+  const getUser = await getModule(["getUser", "getCurrentUser"]);
+
+  inject("bf-user", getUser, "getUser", (args, res) => {
     if (res && this.FAV_FRIENDS.includes(res.id)) {
       const status = getStatus(res.id);
       const previous = this.FRIEND_DATA.statusStorage[res.id];
       if (previous && status !== previous) {
-        this.log('Showing notification');
+        this.log("Showing notification");
         this.createFriendPopup(res, status);
       }
 
